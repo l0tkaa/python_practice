233ec46b59de178e6e818a0b18c94d0636a53e02
l0tkaa <106283020+l0tkaa@users.noreply.github.com>
2022-06-23 21:02:35 -0500

    Add files via upload

---
+++ ex11_test.py
@@ -0,0 +1,8 @@
+print("Pick a number.", end='  ')
+first_number = int(input())
+print("Pick another number.", end='  ')
+second_number = int(input())
+print("Pick your last number.", end='  ')
+third_number = int(input())
+
+print(f"The total of these numbers is {first_number + second_number + third_number}")
